06b760e8d060454519088d8d48f4eaaef996aa90
GerardoAraujo <gerardo.araujos@gmail.com>
2024-02-13 23:37:04 -0500

    portafolioga

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+import parallax from "./components/parallax.js";
+import activeMenu from "./components/selected_menu.js";
+import updateCopyrightYear from "./helpers/date_updater.js";
+import resetToHome from "./helpers/reload_page.js";
+import sendEmail from "./helpers/send__form.js";
+import darkmode from "./helpers/darkmode.js";
+
+parallax()
+
+activeMenu()
+
+resetToHome()
+
+updateCopyrightYear()
+
+sendEmail()
+
+darkmode()
